9cdfc5cebe75e172d71832bf5a482c1c03fd60bb
tomasbgs <49510019+tomasbgs@users.noreply.github.com>
2019-08-25 00:02:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/api/hello", function (req, res) {
 
 app.get(`/api/timestamp/:date_string?`, function (req, res) {
 	console.log(date_string);
-  res.json({greeting: date_string});
+  res.json({greeting: 'date_string'});
 });
 
 
